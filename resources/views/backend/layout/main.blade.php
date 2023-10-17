987e4138d756c0b6aa4702f8f1fed5e238ad18c7
DanishKakar <danishkakar000@gmail.com>
2023-10-17 15:16:15 +0430

    new changes added

---
+++ resources/views/backend/layout/main.blade.php
@@ -0,0 +1,7 @@
+
+@include('backend.layout.head')
+@include('backend.layout.header')
+@include('backend.layout.sidebar')
+@yield('main-content')
+@include('backend.layout.footer')
+@include('backend.layout.script')
